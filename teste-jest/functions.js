64a54621f3dc543b6361bc243ab3d2e908dcaf42
dexPC <bdpaulo@hotmail.com>
2020-01-20 12:31:34 -0300

    Iniciando estudos sobre jest e testes com js

---
+++ teste-jest/functions.js
@@ -0,0 +1,13 @@
+const functions = {
+    add: (num1, num2) => num1 + num2,
+    isNull: () => null,
+    checkValue: x => x,
+    createUser: () => {
+        return user = {
+            firstName: '',
+            lastName: ''
+        }
+    }
+}
+
+module.exports = functions;
