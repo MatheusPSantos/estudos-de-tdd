8278bb9310b83cc6184ca0bd29d62370c7f67e7c
Gabriel Barreto <gsbarretobr@gmail.com>
2020-01-21 10:56:46 -0300

    install axios

--- teste-jest/functions.test.js
+++ teste-jest/functions.test.js
@@ -1,46 +1,46 @@
-const functions = require('./functions');
+const functions = require("./functions");
 
 // primeiro teste
-test('Add 2 + 2 to equal 4', () => {
-    expect(functions.add(2, 2)).toBe(4);
+test("Add 2 + 2 to equal 4", () => {
+  expect(functions.add(2, 2)).toBe(4);
 });
 
 // segundo teste
 // toBeNull matchs only null
-test('Should Be Null', () => {
-    expect(functions.isNull()).toBeNull();
+test("Should Be Null", () => {
+  expect(functions.isNull()).toBeNull();
 });
 
 // terceiro teste
 // toBeFalsy matches anything that an if statment treats as false
-test('Should be falsy', () => {
-    expect(functions.checkValue()).toBeFalsy();
+test("Should be falsy", () => {
+  expect(functions.checkValue()).toBeFalsy();
 });
 
 // toEqual compara recursivamente todas as propriedades do objeto instanciado
-test('User should be TestJest TDD object', () => {
-    expect(functions.createUser()).toEqual({ firstName: '', lastName: ''});
+test("User should be TestJest TDD object", () => {
+  expect(functions.createUser()).toEqual({ firstName: "", lastName: "" });
 });
 
 // Lees than e Greater than
-test('Should be under 1600', () => {
-    const load1 = 800;
-    const load2 = 700;
-    expect(load1+ load2).toBeLessThan(1600); //toBeLessThanOrEqual()
+test("Should be under 1600", () => {
+  const load1 = 800;
+  const load2 = 700;
+  expect(load1 + load2).toBeLessThan(1600); //toBeLessThanOrEqual()
 });
 
 // Regex
-test('There is no I in team', () => {
-    expect('team').not.toMatch(/I/i);
+test("There is no I in team", () => {
+  expect("team").not.toMatch(/I/i);
 });
 
 // Arrays
-test('Should be admin in usernames', () => {
-    const usernames = ['admin', 'Katty', 'Alock', 'Master']
-    expect(usernames).toContain('admin');
+test("Should be admin in usernames", () => {
+  const usernames = ["admin", "Katty", "Alock", "Master"];
+  expect(usernames).toContain("admin");
 });
+
 // Checar para TRUTHY & FALSE Values
 
 // toBeUndefined matchs only undefined
 // toBeTruthy matches anything that an if statement treats as true
-
